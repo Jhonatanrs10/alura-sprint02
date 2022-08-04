1d9c2764c7cd2d8e7df3d8ee3ae32b4db04bc740
jhonatanrs10 <jhonatanrs10@hotmail.com>
2022-08-03 21:36:52 -0300

    implementando funcionalidade criar palavra secreta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Funções
+
 function desenhaTelaJogo(){
     var telaJogo = document.querySelector("canvas");
     var pincel = telaJogo.getContext("2d");
@@ -5,4 +7,29 @@ function desenhaTelaJogo(){
     pincel.fillRect(0, 0, 1200, 800); 
 }
 
+function criarPalavraSecreta(novaPalavra, array){
+    var novaPalavra = "exemplo";
+    array.push(novaPalavra);
+}
+
+var palavrasSecretas = [
+    "ALURA",
+    "JOGO",
+    "CODING",
+];
+
+
+// Elementos HTML
+
+var btCriarPalavra = document.querySelector(".btCriarPalavra");
+
+
+
+// Corpo do Programa
+
 desenhaTelaJogo();
+
+var i = 0;
+btCriarPalavra.addEventListener('click', function() {
+   
+});
