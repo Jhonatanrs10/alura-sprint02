589eea3aa8edf01a45eca2c3c3cf482794d6e88a
jhonatanrs10 <jhonatanrs10@hotmail.com>
2022-08-03 22:01:02 -0300

    implementando botoes interativos

--- js/script.js
+++ js/script.js
@@ -7,29 +7,40 @@ function desenhaTelaJogo(){
     pincel.fillRect(0, 0, 1200, 800); 
 }
 
-function criarPalavraSecreta(novaPalavra, array){
-    var novaPalavra = "exemplo";
-    array.push(novaPalavra);
+function criarPalavraSecreta(){
+    novaPalavra = inNovaPalavra.value;
+    palavrasSecretas.push(novaPalavra.toUpperCase());
+    console.log(palavrasSecretas);
 }
+
+function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+        ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
+        (charCode < 97 || charCode > 122)) {
+        alert("Somente letras por favor.");
+        return false;
+    }
+    return true;
+}
+
+function adcionarNovaPalavra(){
+
+}
+
+// Elementos HTML
+
+var btCriarPalavra = document.querySelector(".btCriarPalavra");
+var inNovaPalavra = document.querySelector(".novaPalavra__jogo");
+// Corpo do Programa
 
 var palavrasSecretas = [
     "ALURA",
     "JOGO",
     "CODING",
 ];
-
-
-// Elementos HTML
-
-var btCriarPalavra = document.querySelector(".btCriarPalavra");
-
-
-
-// Corpo do Programa
-
 desenhaTelaJogo();
-
-var i = 0;
-btCriarPalavra.addEventListener('click', function() {
-   
-});
+var novaPalavra = "exemplo";
+console.log(palavrasSecretas);
+btCriarPalavra.addEventListener('click', criarPalavraSecreta);
